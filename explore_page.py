8c1487c7199a06c486bf304834c8a1635b24bf01
kameshkotwani <kameshkotwani@outlook.com>
2021-06-13 20:22:48 +0530

    completed model

---
+++ explore_page.py
@@ -0,0 +1,79 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def shorten_categories(categories,cutoff):
+    categorical_map = {}
+    for i in range(len(categories)):
+        if categories.values[i]>=cutoff:
+            categorical_map[categories.index[i]]=categories.index[i]
+        else:
+            categorical_map[categories.index[i]]="Other"
+    return categorical_map
+
+
+def clean_exp(x):
+    if x=="More than 50 years":
+        return 50
+    elif x=='Less than 1 year':
+        return 0.5
+    else:
+        return float(x)
+
+def clean_education(x):
+    if 'Bachelor’s degree' in x:
+        return 'Bachelor’s degree'
+    if 'Master’s degree' in x:
+        return 'Master’s degree'
+    if 'Professional degree' in x or 'Other doctoral' in x:
+        return 'Post grad'
+    return 'Less than a Bachelors'
+
+@st.cache # to store the data and not running it again and again
+def load_data():
+	df  = pd.read_csv("survey_results_public.csv")
+	df = df[['Country','EdLevel','YearsCodePro','Employment','ConvertedComp']] # getting only required values
+	df = df.rename({"ConvertedComp":"Salary"},axis=1) # renaming convertedcomp to salary
+	df = df[df["Salary"].notnull()]
+	df = df.dropna()
+	df = df[df['Employment']=='Employed full-time']
+	df = df.drop('Employment',axis=1)
+	country_map = shorten_categories(df.Country.value_counts(),400)
+	df['Country'] = df['Country'].map(country_map)
+	# Refininf the Salary and removing outliers
+	df = df[df['Salary']<=250000]
+	df = df[df['Salary']>=10000]
+	# Droping the other category
+	df = df[df['Country']!='Other'] # it is case sensitive
+	df["YearsCodePro"]=df['YearsCodePro'].apply(clean_exp)
+	df['EdLevel'] = df['EdLevel'].apply(clean_education)
+	return df
+
+df = load_data()
+
+def show_explore_page():
+	st.title("Explore Salaries!")
+
+	st.write("""
+		### Stack Overflow Developer Survey 2020
+	""")
+	data = df['Country'].value_counts()
+	fig1,ax1 = plt.subplots()
+	ax1.pie(data,labels=data.index,autopct="%1.1f%%",shadow=True,startangle=90)
+	ax1.axis("equal") # to make sure that pie is the circle
+
+	st.write("""#### Data From different countries""")
+	st.pyplot(fig1)
+
+
+	st.write("""#### Mean Salary based on country""")
+	data = df.groupby(["Country"])["Salary"].mean().sort_values(ascending=True)
+	st.bar_chart(data)
+
+
+	st.write("""#### Mean Salary based on Experience""")
+	data = df.groupby(["YearsCodePro"])["Salary"].mean().sort_values(ascending=True)
+	st.line_chart(data)
+
+
+
